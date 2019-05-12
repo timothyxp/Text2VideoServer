b19e3001e05f34f3d1671bda4c5bee425823d2be
NevMem <memlolkek@gmail.com>
2019-05-12 04:50:16 +0300

    Added overlays and ome more features

--- server/maker.py
+++ server/maker.py
@@ -41,7 +41,7 @@ def make():
             print(image_src)
             ints.append(ImageInterval(interval['begin'], interval['end'], interval['text'], image_src))
 
-    res_file = config.maker.make(ints, "none", icon = "downloaded/new_icon.png")
+    res_file = config.maker.make(ints, "none", icon="downloaded/new_icon.png", overlay="downloaded/overlay.png")
 
     return json.dumps({
         'type': 'ok',
