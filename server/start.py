1f1f8824a39c267f9e014cc6e81630135ad2905b
NevMem <memlolkek@gmail.com>
2019-05-18 17:44:19 +0300

    Added some ssl configurations

--- server/start.py
+++ server/start.py
@@ -2,7 +2,7 @@ from server.app import app
 
 
 def main():
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', ssl_context="adhoc")
 
 if __name__ == '__main__':
     main()
