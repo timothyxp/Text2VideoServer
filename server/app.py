c91ff811d800d50631cdce6ef6b82a5db2c33343
NevMem <memlolkek@gmail.com>
2019-05-19 12:05:02 +0300

    Another CORS fix

--- server/app.py
+++ server/app.py
@@ -10,8 +10,9 @@ from server import search
 
 
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+    response.headers['Access-Control-Allow-Methods'] = "DELETE, GET, POST, PUT, OPTIONS"
     # if request.method == 'OPTIONS':
         # response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT, OPTIONS'
         # headers = request.headers.get('Access-Control-Request-Headers')
