6a08cd8647adfbac6ba7ba2a59a8a1fcfb3649c1
NevMem <memlolkek@gmail.com>
2019-05-21 18:16:57 +0300

    New search filter

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ from os import path
 
 DOWNLOAD_PATH = path.join(path.split(path.split(__file__)[0])[0], 'downloaded')
 
-YOUTUBE_FILTER = 'EgQQARgB'
+YOUTUBE_FILTER = 'EgIQAQ%3D%3D'
 
 DEFAULT_EMOTION = "unemotion"
 
