97844b757f720d1692e6c75e3420d0a811de5c4f
NevMem <memlolkek@gmail.com>
2019-05-21 18:33:47 +0300

    Back to the future

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ from os import path
 
 DOWNLOAD_PATH = path.join(path.split(path.split(__file__)[0])[0], 'downloaded')
 
-YOUTUBE_FILTER = 'EgIQAQ%3D%3D'
+YOUTUBE_FILTER = 'EgQQARgB'
 
 DEFAULT_EMOTION = "unemotion"
 
