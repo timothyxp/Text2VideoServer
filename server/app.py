6d4c07507e6f4f4e68ec21edccc6fe8c2d3dec54
NevMem <memlolkek@gmail.com>
2019-05-27 08:52:19 +0300

    New logging feature

--- server/app.py
+++ server/app.py
@@ -2,11 +2,28 @@
 
 from flask import Flask, request
 from flask_socketio import SocketIO
+import json
 
 app = Flask("__app__", static_folder='tmp', static_url_path='')
 socketio = SocketIO(app)
 
 working_status = {}
+try:
+    with open('working-status.json', 'r') as inp:
+        data = inp.read()
+        working_status = json.loads(data)
+except:
+    print('File working-status.json not found')
+if not 'max_video_index' in working_status:
+    working_status['max_video_index'] = 0
+
+def saveWorkingStatus():
+    with open('working-status.json', 'w') as out:
+        out.write(json.dumps(working_status))
+
+def saveTimings(current_id, timings):
+    working_status[current_id]['timings'] = timings
+    saveWorkingStatus() 
 
 from server import main_handler
 from server import maker
